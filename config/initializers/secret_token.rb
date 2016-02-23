996ba56d055372cc12c4a80b49107d16c40bb47b
Ken Ho <ken.ho@hotmail.co.uk>
2016-02-23 14:17:49 +0000

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Homegry::Application.config.secret_key_base = '1a3c812235e27c34c1b7792c161b3d12ac8dc622cc0bbe10ab5c8163cf11f59c8243bf6fa2ee0df28697ce4e9bfdf7cfe18eb6d662108fcb6117b8e0c58b8267'
